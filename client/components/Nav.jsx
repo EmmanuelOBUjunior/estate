0f215deee3376ad49a2d389275bab677b47326b9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-21 18:30:45 +0000

    working on Nav component

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,12 +1,12 @@
 const Nav = () => {
-    
+
   return (
     <nav className='bg-slate-300'>
         <h1 className='flex'>
-            <span>
+            <span className="text-slate-600 font-bold">
                 Cris
             </span>
-            <span>
+            <span className="text-slate-800 font-bold">
                 Estate
             </span>
         </h1>
