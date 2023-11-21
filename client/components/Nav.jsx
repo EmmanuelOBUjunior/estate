6e63669d2196aaf31161a69090bfa5772bfe9d9b
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-21 18:35:46 +0000

    working on navbar components

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,13 +1,13 @@
 const Nav = () => {
 
   return (
-    <nav className='bg-slate-300'>
+    <nav className='bg-slate-300 shadow-md'>
         <div className="mx-auto items-center">
         <h1 className='font-bold flex flex-wrap'>
             <span className="text-slate-600 ">
                 Cris
             </span>
-            <span className="text-slate-800 font-bold">
+            <span className="text-slate-800">
                 Estate
             </span>
         </h1>
