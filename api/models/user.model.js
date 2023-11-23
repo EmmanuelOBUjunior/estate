8b2846f763942fd78a720acafaf3156affd223ad
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-23 12:35:28 +0000

    creating model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose';
 
 const userScehema = mongoose.Schema({
-    
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true,
+        unique: true
+    }
 })
