86650ab7983602853819fbecc4c7dd3317eb94a7
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-23 12:31:42 +0000

    connecting database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI).then(()=>{
     console.log("Database connection established successfully");
