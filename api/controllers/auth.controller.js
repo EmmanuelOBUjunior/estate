7476c20229ed9c0233e26f23ea437b529715ae69
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-23 14:23:10 +0000

    hashing password

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,8 +2,12 @@ import User from '../models/user.model.js'
 
 export const signup = async (req, res)=>{
     const {username, email, password} = req.body;
+    const hashedPassword = 
     const newUser = new User({username, email, password});
-    await newUser.save()
-
-    res.status(201).json("User created successfully")
+    try {
+        await newUser.save()
+        res.status(201).json("User created successfully")
+    } catch (err) {
+        res.status(500).json(err)
+    }
 }
