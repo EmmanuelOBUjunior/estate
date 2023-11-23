bf0b8584f7d308ef5e5732b30f8d58796dc643c5
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-23 12:28:52 +0000

    connecting database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
-mongoose.connect("mongodb+srv://Admin:<password>@social.8djb6.mongodb.net/estate?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://Admin:admin1234@social.8djb6.mongodb.net/estate?retryWrites=true&w=majority").then(()=>{
+    console.log("Database connection established successfully");
+})
 
 const app = express();
 
