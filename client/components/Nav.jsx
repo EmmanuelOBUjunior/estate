e05a1cd3ba08872d8c23c136c76b36772e2242ab
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-23 12:06:58 +0000

    Fixing bugs

---
+++ client/components/Nav.jsx
@@ -0,0 +1,32 @@
+import {FaSearch} from 'react-icons/fa'
+
+const Nav = () => {
+
+  return (
+    <nav className='bg-slate-300 shadow-md'>
+        <div className="flex mx-auto justify-between max-w-6xl items-center p-3">
+        <h1 className='font-bold text-sm sm:text-lg flex flex-wrap'>
+            <span className="text-slate-600 ">
+                Cris
+            </span>
+            <span className="text-slate-800">
+                Estate
+            </span>
+        </h1>
+
+        <form className="bg-slate-200 flex items-center p-2 rounded">
+            <input type="text" placeholder="Search" className="bg-transparent focus:outline-none w-24 sm:w-64"/>
+            <FaSearch className='text-slate-500'/>
+        </form>
+
+        <ul className='flex gap-3'>
+            <li className='text-slate-600 hover:underline hidden sm:inline hover:cursor-pointer'>Home</li>
+            <li className='text-slate-600 hover:underline hidden sm:inline hover:cursor-pointer'>About</li>
+            <li className='text-slate-600 hover:underline hidden sm:inline hover:cursor-pointer'>Sign In</li>
+        </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
