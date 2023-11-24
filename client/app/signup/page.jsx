0b1cff2d0d31be8977fc66fd3ffddc5bceace8d1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-11-24 15:57:50 +0000

    working on signup functionality

--- client/app/signup/page.jsx
+++ client/app/signup/page.jsx
@@ -1,22 +1,47 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+    console.log(formData)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3001/api/auth/signup", {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData)
+    })
+    const data = await res.json()
+
+    console.log(data)
+  }
+  console.log(formData)
   return (
     <section>
       <div className='h-screen justify-center flex items-center'>
         <div className='border border-gray-400 p-6 max-w-md w-full rounded'>
         <h1 className='font-bold text-4xl'>Sign Up</h1>
         <p>Create an account with us</p>
-        <form className='mt-4 flex flex-col'>
+        <form onSubmit = {handleSubmit} className='mt-4 flex flex-col'>
           <label htmlFor="username">Username</label>
-          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="text" id="username" placeholder='Enter username here' />
+          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="text" id="username" placeholder='Enter username here' onChange = {handleChange}/>
           <label htmlFor="email">Email</label>
-          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="email" id="email" placeholder='Enter email here' />
+          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="email" id="email" placeholder='Enter email here' onChange = {handleChange}/>
           <label htmlFor="password">Password</label>
-          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="password" id="username" placeholder='Enter password here' />
+          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="password" id="username" placeholder='Enter password here' onChange = {handleChange}/>
 
-          <button className='bg-slate-800 text-white p-3 rounded-lg mt-2'>Sign Up</button>
+          <button className='bg-slate-800 text-white p-3 rounded-lg mt-2 uppercase'>Sign Up</button>
         </form>
 
         <div className='mt-2 flex gap-2 justify-center'>
