5f7176258d39e01833e75c00befdeda83f0d0265
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-04 09:59:17 +0000

    working on signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -20,6 +20,7 @@ export const signin = async (res, req, next) =>{
         const {email, password} = req.body;
         const validUser = await User.findOne({email})
         if(!validUser) return next(errorHandler(400, "Invalid Credentials"))
+        console.log(validUser.email)
         const validPassword =  bcryptjs.compareSync(password, validUser.password)
         if(!validPassword) return next(errorHandler(400, "Invalid Credentials"));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
