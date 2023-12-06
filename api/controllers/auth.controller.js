30581c4f6ef8ac648f1597f3d39afadb430d49dc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-06 07:34:40 +0000

    testing signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -17,17 +17,16 @@ export const signup = async (req, res, next)=>{
 
 export const signin = async (req, res, next) => {
     const {email, password} = req.body;
-    console.log(email, " and " ,password)
-    // try{
-    //     const validUser = await User.findOne({email})
-    //     if(!validUser) return next(errorHandler(400, "Invalid Credentials"))
-    //     const validPassword =  bcryptjs.compareSync(password, validUser.password)
-    //     if(!validPassword) return next(errorHandler(401, "Invalid Credentials"));
-    //     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
-    //     const {password:pass, ...rest} = validUser 
-    //     res.cookie("access_token", token, {httpOnly:true}).status(200).json(rest)
-    // }catch(err){
-    //     next(err)
-    // }
+    try{
+        const validUser = await User.findOne({email})
+        if(!validUser) return next(errorHandler(400, "Invalid Credentials"))
+        const validPassword =  bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return next(errorHandler(401, "Invalid Credentials"));
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        const {password:pass, ...rest} = validUser 
+        res.cookie("access_token", token, {httpOnly:true}).status(200).json(rest)
+    }catch(err){
+        next(err)
+    }
 
 }
