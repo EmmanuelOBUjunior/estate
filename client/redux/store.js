4a54aca05669d0e591887e39e7cbb815a404a87e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-06 08:06:34 +0000

    working on store logic

--- client/redux/store.js
+++ client/redux/store.js
@@ -3,5 +3,8 @@ import { configureStore } from '@reduxjs/toolkit'
 export const makeStore = () => {
   return configureStore({
     reducer: {},
+    middleware: (buildGetDefaultMiddleware) => buildGetDefaultMiddleware({
+        serializableCheck: false
+    })
   })
 }
