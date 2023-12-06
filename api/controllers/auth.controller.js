7d83a5ad4ec888b2a6ce0199ffa0f69c3edcf4c3
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-06 07:34:07 +0000

    testing signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const signup = async (req, res, next)=>{
     }
 }
 
-export const signin = async (res, req, next) => {
+export const signin = async (req, res, next) => {
     const {email, password} = req.body;
     console.log(email, " and " ,password)
     // try{
