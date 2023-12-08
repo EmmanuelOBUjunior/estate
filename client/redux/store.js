c5e3622af2f7fe45fc1563659bc9529f39c3a098
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-08 17:30:43 +0000

    updating signin page with global state

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,10 +1,12 @@
-'use client'
+
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from "./features/user/userSlice.js"
 
-export const makeStore = configureStore({
+export const makeStore = () => {
+  return configureStore({
     reducer: {user: userReducer},
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({
         serializableCheck: false
     })
   })
+}
