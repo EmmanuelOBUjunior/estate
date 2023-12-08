a739e7b6193a6b134792cbd05992f6c035882825
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-08 17:49:51 +0000

    updating signin page with global state

--- client/redux/StoreProvider.jsx
+++ client/redux/StoreProvider.jsx
@@ -1,15 +1,14 @@
 'use client'
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
-import { makeStore } from '../lib/store'
-import userReducer from "./user/userSlice.js"
+import { makeStore } from '../redux/store.js'
+import userReducer from "./features/user/userSlice.js"
 
-export default function StoreProvider({ user, children }) {
+export default function StoreProvider({children }) {
   const storeRef = useRef(null)
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore()
-    storeRef.current.dispatch(userReducer(user))
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>
