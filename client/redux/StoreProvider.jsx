402d80a9f8ff49131456fe128e524d108a929d87
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-08 19:31:22 +0000

    adding redux persist

--- client/redux/StoreProvider.jsx
+++ client/redux/StoreProvider.jsx
@@ -3,13 +3,6 @@ import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore } from '../redux/store.js'
 
-export default function StoreProvider({children }) {
-  const storeRef = useRef(null)
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore
-
-  }
-
+export default function StoreProvider({children}) {
   return <Provider store={storeRef.current}>{children}</Provider>
 }
