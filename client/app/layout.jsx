e78746bbee0db6608cbaa7356196b64da76fc8ef
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-08 19:36:29 +0000

    adding redux persist

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -2,8 +2,8 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Nav from '@/components/Nav'
 import StoreProvider from '@/redux/StoreProvider'
-import { makeStore, persistor } from '@/redux/store.js'
-import { PersistGate } from 'redux-persist/integration/react'
+// import { makeStore, persistor } from '@/redux/store.js'
+// import { PersistGate } from 'redux-persist/integration/react'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Nav/>
-        <StoreProvider>
+        <StoreProvider store = {makeStore}>
         {/* <PersistGate loading={null} persistor={persistor}> */}
         {children}
         {/* </PersistGate> */}
