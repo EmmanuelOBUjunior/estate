0119a1eea5724dcfb10c3fed7761094274087442
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-08 17:06:06 +0000

    updating signin page with global state

--- client/app/signin/page.jsx
+++ client/app/signin/page.jsx
@@ -2,11 +2,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
+import {signinStart, signinSuccess, signinFailure} from ""
+import { useDispatch } from 'react-redux'
 
 const SignIn = () => {
   const [formData, setFormData] = useState({})
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
+  const 
   const router = useRouter()
 
 
