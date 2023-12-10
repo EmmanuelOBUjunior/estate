76e12f137167848de2758ab11ed96098daaad72c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-10 13:35:26 +0000

    fixing redux-bug

--- client/redux/StoreProvider.jsx
+++ client/redux/StoreProvider.jsx
@@ -3,12 +3,18 @@ import { Provider } from 'react-redux'
 import { makeStore } from '../redux/store.js'
 
 export default function StoreProvider({children}) {
+  return (
+  <Provider store={makeStore}>
+    {children}
+    </Provider>
+  
+  )
+}
+
+
   // const storeRef = useRef(null)
   // if (!storeRef.current) {
   //   // Create the store instance the first time this renders
   //   storeRef.current = makeStore
 
   // }
-
-  return <Provider store={makeStore}>{children}</Provider>
-}
