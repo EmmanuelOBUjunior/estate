54a725d5410acf61f70448fd010bdd47905865fd
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-10 13:29:21 +0000

    fixing redux-bug

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,10 +1,6 @@
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Nav from '@/components/Nav'
-import StoreProvider from '@/redux/StoreProvider'
-import { PersistGate } from 'redux-persist/integration/react'
-import { makeStore} from '@/redux/store.js'
-import { persistStore } from 'redux-persist'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +11,6 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
-  let persistor = persistStore(makeStore) 
   return (
     <html lang="en">
       <body className={inter.className}>
