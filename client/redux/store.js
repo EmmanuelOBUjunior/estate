0a6f9be4e8e466d4e4d6d7f90ee3f2a7a3aed00e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-11 08:23:49 +0000

    fixing local storage

--- client/redux/store.js
+++ client/redux/store.js
@@ -2,24 +2,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from "./features/user/userSlice.js"
 import { persistReducer} from 'redux-persist'
-import createWebStorage from "redux-persist/lib/storage/createWebStorage";
-
-const createNoopStorage = () => {
-  return {
-    getItem(_key) {
-      return Promise.resolve(null);
-    },
-    setItem(_key, value) {
-      return Promise.resolve(value);
-    },
-    removeItem(_key) {
-      return Promise.resolve();
-    },
-  };
-};
-
-const storage =
-  typeof window === "undefined" ? createNoopStorage("local") : createWebStorage();
+import storage from 'redux-persist/lib/storage'
 
 
 const rootReducer = combineReducers({user: userReducer})
