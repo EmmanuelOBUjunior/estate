d9d6e8e28e454ac84234e3ff9437036b16d2357d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 21:23:36 +0000

    updating image in firebase

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux'
 import { useRef } from 'react'
 
 const Profile = () => {
-  
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) =>state.user)
   return (
     <section>
