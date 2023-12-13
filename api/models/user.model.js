1252c0a0b8db921fc318e456ed4ec5b67d0ca51e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 04:55:11 +0000

    added avatar field to user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,10 @@ const userScehema = mongoose.Schema({
     password:{
         type: String,
         required: true,
+    },
+    avatar:{
+        type: String,
+        default: "https://imgs.search.brave.com/mDdtZ12xiGTjupVmFywXaqmw7taeD-L12YCXsD02hPQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzA3LzQzLzQ1/LzM2MF9GXzEwNzQz/NDUxMV9pYXJGMno4/OGM2RHM2QWxndHdv/dEhTQWt0V0NkWU9u/Ny5qcGc"
     }
 }, {timeStamps: true})
 
