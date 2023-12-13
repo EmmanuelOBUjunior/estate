712df1710e1d876d12c321580f191da97571d217
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 04:07:45 +0000

    added google auth to OAuth Button

--- client/firebase.js
+++ client/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FOR,
+  apiKey: process.env.GOOGLE_AUTH_KEY,
   authDomain: "crisestate-ce4d3.firebaseapp.com",
   projectId: "crisestate-ce4d3",
   storageBucket: "crisestate-ce4d3.appspot.com",
