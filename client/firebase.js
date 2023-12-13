c27f0d41ae308429d13d2989b14dca9bb84e74d2
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 03:37:42 +0000

    configured firebase - google authentication

---
+++ client/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDmSbmOdyUpfwhfK7VsQw6kRTaUbJVRDUY",
+  authDomain: "crisestate-ce4d3.firebaseapp.com",
+  projectId: "crisestate-ce4d3",
+  storageBucket: "crisestate-ce4d3.appspot.com",
+  messagingSenderId: "795106035743",
+  appId: "1:795106035743:web:f0deb522a7d9479d2c2d2a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
