bc4387bc3ea5adadc617b20e18bcbba732c5ee9d
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 21:00:43 +0000

    working on the profile screen

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <section>
+      <div className='h-screen justify-center flex items-center'>
+        <div className='border border-gray-400 p-6 max-w-md w-full rounded'>
+        <h1 className='font-bold text-4xl'>Sign In</h1>
+        <p>Welcome back, get access to your account</p>
+        <form className='mt-4 flex flex-col'>
+          <label htmlFor="email">Email</label>
+          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="email" id="email" placeholder='Enter email here' onChange = {handleChange}/>
+          <label htmlFor="password">Password</label>
+          <input className='p-3 rounded focus:outline-none text-sm mb-3' type="password" id="password" placeholder='Enter password here' onChange = {handleChange}/>
+
+          <button className='bg-slate-800 text-white p-3 rounded-lg mt-2 mb-2 uppercase disabled:bg-slate-500 hover:bg-slate-700'>{loading ? "Loading...": "Sign In"}</button>
+        </form>
+        </div>
+        </div>
+      </section>
   )
 }
 
