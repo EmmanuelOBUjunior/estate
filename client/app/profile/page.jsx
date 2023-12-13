5b9a0c8544270b4622b12ba68ffa16285e1a1c2a
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-13 21:11:04 +0000

    working on the profile screen

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -8,8 +8,10 @@ const Profile = () => {
     <section>
       <div className='h-screen justify-center flex items-center'>
         <div className='border border-gray-400 p-6 max-w-md w-full rounded'>
+        <div className='items-center flex flex-col gap-2'>
         <h1 className='font-bold text-4xl'>Profile</h1>
         <img src={currentUser.avatar} alt="Profile Picture" className='rounded-full w-50 h-50'/>
+        </div>
         <form className='mt-4 flex flex-col'>
           <label htmlFor="email">Email</label>
           <input className='p-3 rounded focus:outline-none text-sm mb-3' type="email" id="email" placeholder='Email'/>
