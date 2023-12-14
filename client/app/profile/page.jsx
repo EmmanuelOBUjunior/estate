4c3d76c9abbdf58d1f1f80af287bd8f5f3f99c11
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-14 19:35:20 +0000

    working on file upload to firebase

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useRef } from 'react'
 
