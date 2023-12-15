43c8ba2f3d8e916d8bfd8d3d700c803d02c825d9
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-15 09:00:01 +0000

    working on file upload to firebase

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -35,7 +35,7 @@ const Profile = () => {
         setFileUploadError(true)
       };
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
-        setFormData(...formData, avatar: downloadURL)
+        setFormData({...formData, avatar: downloadURL})
       })
   }
 
