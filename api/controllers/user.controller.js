884301e8b4b477a9bdc8f764de0023490c890c4f
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-14 18:19:37 +0000

    Working on updating user profile api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,15 @@
 export const test = (req, res)=>{
-    res.send("Controller injected successfully")
+    res.json({
+        message: "Api route is working"
+    })
 }
 
 export const updateUser = (req, res, next)=>{
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You can only change your own account"))
     
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
 }
