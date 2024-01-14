efe4d1bddcc62cd904ff856e0ba7376dc63e5c52
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-01-14 17:45:43 +0000

    Working on updating user profile api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config()
 
@@ -16,6 +18,7 @@ mongoose.connect(process.env.MONGO_URI).then(()=>{
 const app = express();
 //Accept json response
 app.use(express.json());
+app.use(cookieParser())
 
 app.listen(3001, ()=>{
     console.log('Server started successfully on port 3001');
